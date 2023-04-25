4eb9b7b8099ff843dfecb7717ae3d6c75e521e43
Ramsey <ramseyv2@illinois.edu>
2023-04-24 19:39:31 -0500

    fixed dep

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import ECEB from "~/images/ECEB.jpg";
 import Image from "next/image";
-import Typewriter from "typewriter-effect";
 
 export const Landing = () => {
   return (
     <div className="relative flex h-screen w-screen flex-col items-center justify-center">
-      <div className="relative -top-12 z-10 text-white">
+      <div className="relative -top-12 z-10 text-white text-center">
         <h1 className="m-10 text-9xl font-bold">IEEE UIUC</h1>
-        <div className="semi-bold flex flex-col items-center justify-center text-4xl ">
-          <h2>Join us for</h2>
-          <Typewriter
-            options={{
-              strings: [
-                "Seminars",
-                "Technical Workshops",
-                "Socials",
-                "Tech Talks",
-                "and Much More",
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
+        <div className="font-extralight flex flex-col items-center justify-center text-3xl ">
+          <h2 className="text-wrap w-8/12 md:max-w-screen-lg text-center">
+            Join us for tech talks, info sessions, luncheons, workshops, and socials!
+          </h2>
         </div>
       </div>
       <div className="absolute top-0 left-0 z-0 h-screen w-screen ">
