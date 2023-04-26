68b710e01c22ed18b1cf3cb2e57e941d6e890615
Ramsey <ramseyv2@illinois.edu>
2023-04-26 13:03:03 -0500

    only signed in users can send emails

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -21,8 +21,8 @@ export const Contact = () => {
         const response = await axios.post("/api/email", {
           message,
           subject,
-          sender : signedInSender,
-          email : signedInEmail,
+          sender: signedInSender,
+          email: signedInEmail,
         });
         if (response.status === 200) {
           setMessage("");
@@ -60,13 +60,9 @@ export const Contact = () => {
 
   return (
     <div className="relative flex min-h-screen w-screen flex-col items-center justify-center overflow-hidden ">
-      <form
-        onSubmit={SendMail}
-        onReset={ClearForm}
-        className="w-11/12 rounded-xl border-2  border-gray-400 p-6 md:w-6/12"
-      >
-        <h2 className="mb-8 text-6xl font-bold">Contact Us</h2>
-        {!isSignedIn && (
+      {!isSignedIn && (
+        <form className="w-11/12 rounded-xl border-2  border-gray-400 p-6 md:w-6/12">
+          <h2 className="mb-8 text-6xl font-bold">Contact Us</h2>
           <div className="pt-2">
             <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
               Username
@@ -78,35 +74,12 @@ export const Contact = () => {
               <input
                 type="text"
                 name="name"
-                value={sender}
-                required
-                onChange={(e) => setSender(e.target.value)}
                 className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="Your Username"
-              />
-            </div>
-          </div>
-        )}
-        {isSignedIn && (
-          <div className="pt-2">
-            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-              Username
-            </label>
-            <div className="flex">
-              <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
-                @
-              </span>
-              <input
-                type="text"
-                id="website-admin"
-                className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder={user?.username as string}
                 disabled
               />
             </div>
           </div>
-        )}
-        {!isSignedIn && (
           <div className="pt-2">
             <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
               Your Email
@@ -129,11 +102,92 @@ export const Contact = () => {
                 type="email"
                 className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="netid@illinois.edu"
+                disabled
               />
             </div>
           </div>
-        )}
-        {isSignedIn && (
+          <div className="pt-2">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Subject
+            </label>
+            <input
+              name="subject"
+              type="text"
+              placeholder="Please sign in to send a message"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
+              disabled
+            />
+          </div>
+          <div className="mb-6 pt-2">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Message
+            </label>
+            <input
+              name="message"
+              placeholder="Please sign in to send a message"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+              disabled
+            />
+          </div>
+          <div className="mb-6 flex w-full flex-row justify-between">
+            <button
+              disabled
+              type="reset"
+              className="
+            focus:shadow-outline
+            mt-2
+            h-10
+            rounded-lg
+            bg-gray-700
+            px-5
+            text-indigo-100
+          "
+            >
+              Clear
+            </button>
+
+            <button
+              disabled
+              type="submit"
+              className="
+            focus:shadow-outline
+            mt-2
+            h-10
+            rounded-lg
+            bg-gray-700
+            px-5
+            text-indigo-100
+          "
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+      {isSignedIn && (
+        <form
+          onSubmit={SendMail}
+          onReset={ClearForm}
+          className="w-11/12 rounded-xl border-2  border-gray-400 p-6 md:w-6/12"
+        >
+          <h2 className="mb-8 text-6xl font-bold">Contact Us</h2>
+          <div className="pt-2">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Username
+            </label>
+            <div className="flex">
+              <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400">
+                @
+              </span>
+              <input
+                type="text"
+                id="website-admin"
+                className="block w-full min-w-0 flex-1 rounded-none rounded-r-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder={user?.username as string}
+                disabled
+              />
+            </div>
+          </div>
           <div className="pt-2">
             <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
               Your Email
@@ -163,36 +217,35 @@ export const Contact = () => {
               />
             </div>
           </div>
-        )}
-        <div className="pt-2">
-          <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-            Subject
-          </label>
-          <input
-            name="subject"
-            type="text"
-            value={subject}
-            required
-            onChange={(e) => setSubject(e.target.value)}
-            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
-          />
-        </div>
-        <div className="mb-6 pt-2">
-          <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-            Message
-          </label>
-          <input
-            name="message"
-            value={message}
-            required
-            onChange={(e) => setMessage(e.target.value)}
-            className="sm:text-md block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-          />
-        </div>
-        <div className="mb-6 flex w-full flex-row justify-between">
-          <button
-            type="reset"
-            className="
+          <div className="pt-2">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Subject
+            </label>
+            <input
+              name="subject"
+              type="text"
+              value={subject}
+              required
+              onChange={(e) => setSubject(e.target.value)}
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-xs"
+            />
+          </div>
+          <div className="mb-6 pt-2">
+            <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+              Message
+            </label>
+            <input
+              name="message"
+              value={message}
+              required
+              onChange={(e) => setMessage(e.target.value)}
+              className="sm:text-md block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            />
+          </div>
+          <div className="mb-6 flex w-full flex-row justify-between">
+            <button
+              type="reset"
+              className="
             focus:shadow-outline
             mt-2
             h-10
@@ -204,13 +257,13 @@ export const Contact = () => {
             duration-150
             hover:bg-indigo-900
           "
-          >
-            Clear
-          </button>
+            >
+              Clear
+            </button>
 
-          <button
-            type="submit"
-            className="
+            <button
+              type="submit"
+              className="
             focus:shadow-outline
             mt-2
             h-10
@@ -222,11 +275,12 @@ export const Contact = () => {
             duration-150
             hover:bg-indigo-900
           "
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
