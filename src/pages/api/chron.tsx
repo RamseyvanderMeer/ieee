34764077aad083c6e7802749b61948bad423a876
Ramsey <ramvandermeer@gmail.com>
2023-08-21 21:50:47 -0500

    Every minute

--- src/pages/api/chron.tsx
+++ src/pages/api/chron.tsx
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { stdout } from "process";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): void {
+  console.log("Cron job ran!");
+  stdout.write("Cron job ran!");
   res.status(200).end("Hello Cron!");
 }
