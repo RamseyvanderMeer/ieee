5610a18f1543235cd021bc6cd85e4f90f3e776a0
Ramsey <ramvandermeer@gmail.com>
2023-09-16 15:15:12 -0500

    Remove sign up for spacing

--- src/components/aboutsection.tsx
+++ src/components/aboutsection.tsx
@@ -22,11 +22,11 @@ export const AboutSection = () => {
         to seeing you soon!
       </p>
       <div className="flex md:flex-row flex-col">
-        {!isSignedIn && (
+        {/* {!isSignedIn && (
           <div className="m-4 w-full w-[30vw] items-center justify-center rounded border-2 border-white px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
             <SignUpButton />
           </div>
-        )}
+        )} */}
         {isSignedIn && user && (
           <div className="w-full w-[30vw] items-center justify-center rounded p-3 px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
             <Link href={`/@${user.username as string}`}>Profile</Link>
