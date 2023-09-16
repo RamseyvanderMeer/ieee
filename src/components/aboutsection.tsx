7c9b4e3c8abe3b39496f6bdd8cefff59dd159abd
Ramsey <ramvandermeer@gmail.com>
2023-09-16 15:08:42 -0500

    Added join discord

--- src/components/aboutsection.tsx
+++ src/components/aboutsection.tsx
@@ -17,20 +17,25 @@ export const AboutSection = () => {
         information on internships. Forge lasting connections with like-minded
         peers through our socials. Explore various topics through our engaging
         workshops. Plus, TAGS hosts recurring meetings to help you hone
-        essential industry skills. Don&rsquo;t miss out on our other exciting events –
-        stay updated through our weekly email updates. We look forward to seeing
-        you soon!
+        essential industry skills. Don&rsquo;t miss out on our other exciting
+        events – stay updated through our weekly email updates. We look forward
+        to seeing you soon!
       </p>
-      {!isSignedIn && (
-        <div className="m-4 w-full w-[30vw] items-center justify-center rounded border-2 border-white px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
-          <SignUpButton />
+      <div className="flex md:flex-row flex-col">
+        {!isSignedIn && (
+          <div className="m-4 w-full w-[30vw] items-center justify-center rounded border-2 border-white px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
+            <SignUpButton />
+          </div>
+        )}
+        {isSignedIn && user && (
+          <div className="w-full w-[30vw] items-center justify-center rounded p-3 px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
+            <Link href={`/@${user.username as string}`}>Profile</Link>
+          </div>
+        )}
+        <div className="w-[30vw w-full items-center justify-center rounded p-3 px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
+          <Link href="https://discord.gg/Gc9qPBxzbS">Join Discord</Link>
         </div>
-      )}
-      {isSignedIn && user && (
-        <div className="w-full w-[30vw] items-center justify-center rounded p-3 px-3 py-2 font-bold text-white hover:bg-slate-800 hover:text-white md:inline-flex md:w-auto">
-          <Link href={`/@${user.username as string}`}>Profile</Link>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
